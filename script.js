100ad6c31017be20179ae3af24d537a70d33390b
anjetech <anjenieuwenhuis@gmail.com>
2026-02-19 12:08:47 +0200

    adding new changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth scroll for navigation
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
